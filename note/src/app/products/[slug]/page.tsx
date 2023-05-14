57a412f3393e22fe927d136aab543619cf2b6198
hajin <hajinkim@kangwon.ac.kr>
2023-05-14 23:02:21 +0900

    chapter 5.11 fetch를 사용한 CSR

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -2,32 +2,32 @@ import { getProduct, getProducts } from '@/service/products';
 import { notFound } from 'next/navigation';
 
 type Props = {
-  params: {
-    slug: string;
-  };
+	params: {
+		slug: string;
+	};
 };
-export const revalidate= 4;
+export const revalidate = 4;
 
 export function generateMetadata({ params }: Props) {
-  return {
-    title: `제품의 이름: ${params.slug}`,
-  };
+	return {
+		title: `제품의 이름: ${params.slug}`,
+	};
 }
 
 export default async function ProductPage({ params: { slug } }: Props) {
-  const product = await getProduct(slug);
+	const product = await getProduct(slug);
 
-  if (!product) {
-    notFound();
-  }
+	if (!product) {
+		notFound();
+	}
 
-  return <h1>{product.name} 제품 설명 페이지</h1>;
+	return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
 export async function generateStaticParams() {
-  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
-  const products = await getProducts();
-  return products.map((product) => ({
-    slug: product.id,
-  }));
+	// 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+	const products = await getProducts();
+	return products.map(product => ({
+		slug: product.id,
+	}));
 }
--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,19 +1,21 @@
+import MeowArticle from '@/components/MeowArticle';
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 
-export const revalidate= 4;
 export default async function ProductsPage() {
-  const products = await getProducts();
-  return (
-    <>
-      <h1>제품 소개 페이지!</h1>
-      <ul>
-        {products.map(({ id, name }, index) => (
-          <li key={index}>
-            <Link href={`/products/${id}`}>{name}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+	const products = await getProducts();
+
+	return (
+		<>
+			<h1>제품 소개 페이지!</h1>
+			<ul>
+				{products.map(({ id, name }, index) => (
+					<li key={index}>
+						<Link href={`/products/${id}`}>{name}</Link>
+					</li>
+				))}
+			</ul>
+			<MeowArticle />
+		</>
+	);
 }

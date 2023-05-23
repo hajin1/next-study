f863be679bf69b7c61aa58de43b473c98a4f47de
hajin <hajinkim@kangwon.ac.kr>
2023-05-23 23:29:27 +0900

    redirect, rewrite, useRouter

--- note/next.config.js
+++ note/next.config.js
@@ -11,6 +11,36 @@ const nextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/products/deleted_forever',
+				destination: '/products',
+				permanent: true, // 308 Permanent Redirect (디스크 캐시에서)
+			},
+			{
+				source: '/products/deleted_temp',
+				destination: '/products',
+				permanent: false, // 307 Temporary Redirect
+			},
+		];
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/hajin',
+				destination: '/about/me/hajin',
+			},
+			{
+				source: '/item',
+				destination: '/products/1234',
+			},
+			{
+				source: '/item/:slug',
+				destination: '/products/:slug',
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,6 +1,7 @@
+import GoProductsButton from '@/components/GoProductsButton';
 import { getProduct, getProducts } from '@/service/products';
 import Image from 'next/image';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 type Props = {
 	params: {
@@ -19,13 +20,15 @@ export default async function ProductPage({ params: { slug } }: Props) {
 	const product = await getProduct(slug);
 
 	if (!product) {
-		notFound();
+		// notFound();
+		redirect('/products');
 	}
 
 	return (
 		<>
 			<h1>{product.name} 제품 설명 페이지</h1>
 			<Image src={`/images/${product.image}`} alt={product.name} width={400} height={300} />
+			<GoProductsButton />
 		</>
 	);
 }
